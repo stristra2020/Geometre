c3153edfb486abee47ca2c5b7e6a6168d8bce55b
stristra2020 <154002881+stristra2020@users.noreply.github.com>
2024-12-26 18:22:12 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1735218577,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Спрайт4-sheet0.png",
+		"images/Спрайт5-sheet0.png",
+		"images/Спрайт6-sheet0.png",
+		"images/Спрайт9-sheet0.png",
+		"images/Спрайт10-sheet0.png",
+		"images/Спрайт13-sheet0.png",
+		"images/Спрайт14-sheet0.png",
+		"images/Спрайт15-sheet0.png",
+		"images/ПлиточныйФон.png",
+		"images/ПлиточныйФон2.png",
+		"images/Спрайт16-sheet0.png",
+		"images/Спрайт17-sheet0.png",
+		"images/Частицы.png",
+		"images/Спрайт18-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
